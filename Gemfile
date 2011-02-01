bf650ef3e31ee19a412b82fce31f3fe273867eda
Leandro Marcucci <leanucci@gmail.com>
2011-02-01 19:26:27 -0300

    Wrapped up basic functionality.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "http://rubygems.org"
-gem "savon"
+gem "savon", "~> 0.7.8"
 
 group :development do
   gem "ruby-debug"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
 require 'bravo'
+require 'rspec'
+
+class SpecHelper
+  include Savon::Logger
+  log = false
+end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -11,5 +16,12 @@ RSpec.configure do |config|
 
 end
 
-Bravo.pkey = 'spec/fixtures/pkey'
-Bravo.cert = 'spec/fixtures/cert.crt'
+Bravo.pkey = "spec/fixtures/pkey"
+Bravo.cert = "spec/fixtures/cert.crt"
+Bravo.cuit = "30711034389"
+Bravo.sale_point = "2"
+Bravo.service_url = "http://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
+Bravo.default_concept = 2
+Bravo.default_doc_type = 0
+Bravo.default_mon_id = 0
+Bravo.own_iva_cond = "RI"
