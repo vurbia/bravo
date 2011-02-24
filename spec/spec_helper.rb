acc92de3a547ee1428222eb9b91d2ff630724038
Emilio Tagua <miloops@gmail.com>
2011-02-24 17:50:32 -0300

    Refactor currencies usage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,5 +23,5 @@ Bravo.sale_point = "0002"
 Bravo.service_url = "http://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
 Bravo.default_concept = 2  # prod y ss
 Bravo.default_doc_type = 0 # cuit
-Bravo.default_mon_id = 0   # pesos
+Bravo.default_moneda = :peso
 Bravo.own_iva_cond = :responsable_inscripto
