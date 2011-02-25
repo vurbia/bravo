351105bee9192aeef120ac20506a0f7192eaed3c
Emilio Tagua <miloops@gmail.com>
2011-02-25 11:44:02 -0300

    Clean this up.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'bravo'
 require 'rspec'
 
