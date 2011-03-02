eb3b9bd4f2f5caaa962d7bf3aeecbdcea170968f
Leandro Marcucci <leanucci@gmail.com>
2011-03-02 13:44:20 -0300

    CUIT is set through env variable for testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ Savon::Request.log = false unless ENV["VERBOSE"] == "true"
 
 Bravo.pkey = "spec/fixtures/pkey"
 Bravo.cert = "spec/fixtures/cert.crt"
-Bravo.cuit = "30711034389"
+Bravo.cuit = ENV["CUIT"] || raise(Bravo::NullOrInvalidAttribute.new, "Please set CUIT env variable.")
 Bravo.sale_point = "0002"
 Bravo.service_url = "http://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
 Bravo.default_concepto = "Productos y Servicios"
