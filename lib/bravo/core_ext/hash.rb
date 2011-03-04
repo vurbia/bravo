12321c2d0510c1fe1a57a1b648826a3a345f78a1
Leandro Marcucci <leanucci@gmail.com>
2011-03-04 00:23:21 -0300

    Added symbolize_keys to Hash.

--- lib/bravo/core_ext/hash.rb
+++ lib/bravo/core_ext/hash.rb
@@ -9,4 +9,15 @@ class Hash
   def symbolize_keys
     dup.symbolize_keys!
   end
+
+  def underscore_keys!
+    keys.each do |key|
+      self[(key.downcase rescue key) || key] = delete(key)
+    end
+    self
+  end
+
+  def underscore_keys
+    dup.underscore_keys!
+  end
 end
